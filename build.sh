ec7bb289690ff23bab0b90dd4a82ac742f0fd6f4
Abhishek Thummar <50793433+Abhishek91520@users.noreply.github.com>
2026-04-14 09:49:51 +0530

    Add package installation commands to build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+apt-get update && apt-get install -y \
+    libpq-dev gcc python3-dev \
+    libxml2-dev libxslt1-dev \
+    zlib1g-dev libsasl2-dev \
+    libldap2-dev libssl-dev \
+    libjpeg-dev libpng-dev
